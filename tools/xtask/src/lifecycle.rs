9514e293176eaf609dfb9b9dfee7f0f6684f5866
Ai Jun <aijun@outlook.com>
2026-08-21 16:33:46 +0800

    record-rfc-0005-completion

--- tools/xtask/src/lifecycle.rs
+++ tools/xtask/src/lifecycle.rs
@@ -971,7 +971,7 @@ superseded_by = ""
                 .iter()
                 .filter(|record| record.status == "Accepted")
                 .count(),
-            30
+            31
         );
         assert_eq!(
             registry
